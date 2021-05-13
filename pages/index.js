3c065559d57cb2dc3d21ab3d5c046a8e130ed2f9
Daniel Stocks <daniel@webcloud.se>
2021-05-13 12:53:09 +0200

    Update index.js (#24)

--- pages/index.js
+++ pages/index.js
@@ -51,9 +51,11 @@ const Home = () => (
         as="p"
         extend={{ "& svg": { verticalAlign: "text-bottom" } }}
       >
-        I'm Daniel and this is my personal website. I specialise in 
-        {" "}<Link href="/cv">working</Link> with UI Engineering & Digital Product Design. If you wish to
-        get in touch please send an email to{" "}
+        I'm Daniel and this is my personal website. I <Link href="/cv">specialise</Link> in 
+        {" "} creating software.
+  
+        I'm currently occupied improving the <Link href="https://www.carla.se/">online car buying and leasing experience</Link> for electrified vehicles.{" "}
+        If you wish to get in touch please send an email to{" "}
         <Link href="mailto:daniel@webcloud.se">daniel@webcloud.se</Link>
         <br />
         or find me on{" "}
